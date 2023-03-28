4bc612473f28453b95772c5ba3b64379f11da147
gre-v-el <myszkier.gabriel@gmail.com>
2023-03-28 18:01:27 +0200

    Encapsulated camera and added drag controls

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+mod camera;
+
 use std::collections::HashMap;
 
+use camera::OrbitCamera;
 use egui_macroquad::{egui::{self, ecolor::rgb_from_hsv}, macroquad::{self, prelude::*}};
 
 fn lorenz(pos: &mut (f64, f64, f64), sigma: f64, rho: f64, beta: f64, dt: f64) {
@@ -12,56 +15,44 @@ fn lorenz(pos: &mut (f64, f64, f64), sigma: f64, rho: f64, beta: f64, dt: f64) {
 	pos.2 += dt*dz;
 }
 
-// fn spawn_seeds(position: Vec<(f64, f64, f64)>, x: f64, y: f64, z: f64, )
+fn spawn_seeds(positions: &mut Vec<(f64, f64, f64)>, cx: f64, cy: f64, cz: f64, radius: f64, amount: usize) {
+	for x in 0..amount {
+		let x = x as f64 / (amount-1) as f64 * radius*2.0 - radius;
+
+		for y in 0..amount {
+			let y = y as f64 / (amount-1) as f64 * radius*2.0 - radius;
+
+			for z in 0..amount {
+				let z = z as f64 / (amount-1) as f64 * radius*2.0 - radius;
+
+				positions.push((x + cx, y + cy, z + cz));
+			}
+		}
+	}
+}
 
 #[macroquad::main("chaotic attractors")]
 async fn main() {
 	let mut attractor = HashMap::new();
 
 	let mut positions = Vec::new();
+	spawn_seeds(&mut positions, 1.0, 1.0, 10.0, 0.1, 10);
 
-	let mut polar: f32 = 0.5;
-	let mut azimuth: f32 = 0.3;
-	let mut zoom: f32 = 50.0;
-
-	let sensitivity = 3.0;
-	let mut last_mouse = Vec2::from(mouse_position()) / vec2(screen_width(), screen_width());
-
-	let resolution = 20;
-	let amplitude = 0.1;
-	for x in 0..resolution {
-		let x = x as f64 / (resolution-1) as f64 * amplitude*2.0 - amplitude;
-
-		for y in 0..resolution {
-			let y = y as f64 / (resolution-1) as f64 * amplitude*2.0 - amplitude;
-
-			for z in 0..resolution {
-				let z = z as f64 / (resolution-1) as f64 * amplitude*2.0 - amplitude;
-
-				positions.push((x, y, z));
-			}
-		}
-	}
+	let mut camera = OrbitCamera {
+		center: vec3(1.0, 1.0, 10.0),
+		polar: 0.3,
+		azimuth: 0.5,
+		zoom: -20.0,
+		rotate_sinsitivity: 3.0,
+		last_mouse: Vec2::from(mouse_position()) / vec2(screen_width(), screen_width()),
+	};
 
 	loop {
-		let mouse = Vec2::from(mouse_position()) / vec2(screen_width(), screen_width());
 		clear_background(BLACK);
 
-		if is_mouse_button_down(MouseButton::Left) {
-			let delta = mouse - last_mouse;
-			azimuth -= delta.x * sensitivity;
-			polar -= delta.y * sensitivity;
-		}
-		zoom *= 0.999f32.powf(mouse_wheel().1);
+		camera.update();
 
-		set_camera(&Camera3D {
-			position: Vec3 { 
-				x: zoom * polar.sin() * azimuth.cos(),
-				y: zoom * polar.sin() * azimuth.sin(),
-				z: zoom * polar.cos(),
-			},
-			..Default::default()
-		});
+		set_camera(&camera.camera());
 
 		for pos in &mut positions {
 			let key = (pos.0 as i32, pos.1 as i32, pos.2 as i32);
@@ -70,11 +61,10 @@ async fn main() {
 			draw_cube(vec3(pos.0 as f32, pos.1 as f32, pos.2 as f32), Vec3::splat(0.3), None, ORANGE);
 		}
 
-		for (pos, density) in &attractor {
-			draw_cube(vec3(pos.0 as f32, pos.1 as f32, pos.2 as f32), Vec3::splat(1.0), None, Color{r: 0.1, g: 0.2, b: 1.0, a: 1.0});
-		}
+		// for (pos, density) in &attractor {
+		// 	draw_cube(vec3(pos.0 as f32, pos.1 as f32, pos.2 as f32), Vec3::splat(1.0), None, Color{r: 0.1, g: 0.2, b: 1.0, a: 1.0});
+		// }
 
-		last_mouse = mouse;
 		next_frame().await;
 	}	
 }
