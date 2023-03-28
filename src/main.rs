c603df08cd18d7af43ba3916ea60bed2c0ba5f19
gre-v-el <myszkier.gabriel@gmail.com>
2023-03-28 17:30:43 +0200

    Added the lorenz attractor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use std::collections::HashMap;
 
-use egui_macroquad::{egui, macroquad::{self, prelude::*}};
+use egui_macroquad::{egui::{self, ecolor::rgb_from_hsv}, macroquad::{self, prelude::*}};
+
+fn lorenz(pos: &mut (f64, f64, f64), sigma: f64, rho: f64, beta: f64, dt: f64) {
+	let dx = sigma * (pos.1 - pos.0);
+	let dy = pos.0 * (rho - pos.2) - pos.1;
+	let dz = pos.0 * pos.1 - beta * pos.2;
+
+	pos.0 += dt*dx;
+	pos.1 += dt*dy;
+	pos.2 += dt*dz;
+}
+
+// fn spawn_seeds(position: Vec<(f64, f64, f64)>, x: f64, y: f64, z: f64, )
 
 #[macroquad::main("chaotic attractors")]
 async fn main() {
-	// let attractor = HashMap::new();
+	let mut attractor = HashMap::new();
 
 	let mut positions = Vec::new();
 
@@ -15,16 +27,16 @@ async fn main() {
 	let sensitivity = 3.0;
 	let mut last_mouse = Vec2::from(mouse_position()) / vec2(screen_width(), screen_width());
 
-	let resolution = 4;
-	let amplitude = 10.0;
+	let resolution = 20;
+	let amplitude = 0.1;
 	for x in 0..resolution {
-		let x = x as f64 / resolution as f64 * amplitude;
+		let x = x as f64 / (resolution-1) as f64 * amplitude*2.0 - amplitude;
 
 		for y in 0..resolution {
-			let y = y as f64 / resolution as f64 * amplitude;
+			let y = y as f64 / (resolution-1) as f64 * amplitude*2.0 - amplitude;
 
 			for z in 0..resolution {
-				let z = z as f64 / resolution as f64 * amplitude;
+				let z = z as f64 / (resolution-1) as f64 * amplitude*2.0 - amplitude;
 
 				positions.push((x, y, z));
 			}
@@ -40,6 +52,7 @@ async fn main() {
 			azimuth -= delta.x * sensitivity;
 			polar -= delta.y * sensitivity;
 		}
+		zoom *= 0.999f32.powf(mouse_wheel().1);
 
 		set_camera(&Camera3D {
 			position: Vec3 { 
@@ -50,11 +63,18 @@ async fn main() {
 			..Default::default()
 		});
 
-		for pos in &positions {
+		for pos in &mut positions {
+			let key = (pos.0 as i32, pos.1 as i32, pos.2 as i32);
+			attractor.insert(key, attractor.get(&key).unwrap_or(&0) + 1);
+			lorenz(pos, 10.0, 28.0, 8.0/3.0, 0.01);
 			draw_cube(vec3(pos.0 as f32, pos.1 as f32, pos.2 as f32), Vec3::splat(0.3), None, ORANGE);
 		}
 
-		last_mouse = Vec2::from(mouse_position()) / vec2(screen_width(), screen_width());
+		for (pos, density) in &attractor {
+			draw_cube(vec3(pos.0 as f32, pos.1 as f32, pos.2 as f32), Vec3::splat(1.0), None, Color{r: 0.1, g: 0.2, b: 1.0, a: 1.0});
+		}
+
+		last_mouse = mouse;
 		next_frame().await;
 	}	
 }
