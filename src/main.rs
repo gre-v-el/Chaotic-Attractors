dbd39a09e41dbdbb4c25cdf90ed64b25f56db326
gre-v-el <myszkier.gabriel@gmail.com>
2023-03-28 07:26:01 +0200

    Added 3d camera and some controls

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,16 @@ use egui_macroquad::{egui, macroquad::{self, prelude::*}};
 
 #[macroquad::main("chaotic attractors")]
 async fn main() {
-	let attractor = HashMap::new();
+	// let attractor = HashMap::new();
 
-	let positions = Vec::new();
+	let mut positions = Vec::new();
+
+	let mut polar: f32 = 0.5;
+	let mut azimuth: f32 = 0.3;
+	let mut zoom: f32 = 50.0;
+
+	let sensitivity = 3.0;
+	let mut last_mouse = Vec2::from(mouse_position()) / vec2(screen_width(), screen_width());
 
 	let resolution = 4;
 	let amplitude = 10.0;
@@ -25,12 +32,29 @@ async fn main() {
 	}
 
 	loop {
+		let mouse = Vec2::from(mouse_position()) / vec2(screen_width(), screen_width());
 		clear_background(BLACK);
 
-		// set_camera(&Camera3D {
+		if is_mouse_button_down(MouseButton::Left) {
+			let delta = mouse - last_mouse;
+			azimuth -= delta.x * sensitivity;
+			polar -= delta.y * sensitivity;
+		}
 
-		// });
+		set_camera(&Camera3D {
+			position: Vec3 { 
+				x: zoom * polar.sin() * azimuth.cos(),
+				y: zoom * polar.sin() * azimuth.sin(),
+				z: zoom * polar.cos(),
+			},
+			..Default::default()
+		});
 
+		for pos in &positions {
+			draw_cube(vec3(pos.0 as f32, pos.1 as f32, pos.2 as f32), Vec3::splat(0.3), None, ORANGE);
+		}
+
+		last_mouse = Vec2::from(mouse_position()) / vec2(screen_width(), screen_width());
 		next_frame().await;
 	}	
 }
